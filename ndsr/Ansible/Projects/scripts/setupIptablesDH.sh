3fefb4485493c503097680cd80f49f405d25c656
v.urusov <v.urusov@ndmsystems.com>
2024-05-16 17:29:57 +0300

    Изменение портов для подключения к ssh и vnc на контейнере

--- ndsr/Ansible/Projects/scripts/setupIptablesDH.sh
+++ ndsr/Ansible/Projects/scripts/setupIptablesDH.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+SSH_PORTS_DEFAULT=22
+VNC_PORTS_DEFAULT=6080
 MODELS=$(docker ps -a --format '{{json .}}' |  jq -r .Names)
 IFS=$'\n' readarray -t MODELS <<< "$MODELS"
 if [ -z "$MODELS" ];then
@@ -28,19 +30,19 @@ for MODEL in "${!MODELS[@]}"; do
 if  sudo iptables -t nat -S -v | grep -E -q  "to-destination ${IPv4_ADDRESS[$MODEL]}:${WEB_PORTS[$MODEL]}"; then
 :
 else
-        sudo iptables -t nat -A PREROUTING -i internet -p tcp --dport ${WEB_PORTS[$MODEL]} -j DNAT --to-destination ${IPv4_ADDRESS[$MODEL]}:${WEB_PORTS[$MODEL]}
+        sudo iptables -t nat -A PREROUTING -i internet -p tcp --dport ${WEB_PORTS[$MODEL]} -j DNAT --to-destination ${IPv4_ADDRESS[$MODEL]}:${WEB_PORTS[$MOEL]}
 fi
 
 if  sudo iptables -t nat -S -v | grep -E -q  "to-destination ${IPv4_ADDRESS[$MODEL]}:${SSH_PORTS[$MODEL]}"; then
 :
 else
-         sudo iptables -t nat -A PREROUTING -i internet -p tcp --dport ${SSH_PORTS[$MODEL]} -j DNAT --to-destination ${IPv4_ADDRESS[$MODEL]}:${SSH_PORTS[$MODEL]}
+         sudo iptables -t nat -A PREROUTING -i internet -p tcp --dport ${SSH_PORTS[$MODEL]} -j DNAT --to-destination ${IPv4_ADDRESS[$MODEL]}:${SSH_PORTS_DEFAULT}
 fi
 
 if  sudo iptables -t nat -S -v | grep -E -q  "to-destination ${IPv4_ADDRESS[$MODEL]}:${VNC_PORTS[$MODEL]}"; then
 :
 else
-        sudo iptables -t nat -A PREROUTING -i internet -p tcp --dport ${VNC_PORTS[$MODEL]} -j DNAT --to-destination ${IPv4_ADDRESS[$MODEL]}:${VNC_PORTS[$MODEL]}
+        sudo iptables -t nat -A PREROUTING -i internet -p tcp --dport ${VNC_PORTS[$MODEL]} -j DNAT --to-destination ${IPv4_ADDRESS[$MODEL]}:${VNC_PORTS_DEFAULT}
 fi
 
 done
