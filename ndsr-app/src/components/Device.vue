15bbacf5bb6e66c4be07e7e639e5abcedc1fa117
v.urusov <v.urusov@ndmsystems.com>
2024-05-31 18:00:34 +0300

    разделение кнопок(исправвление)

--- ndsr-app/src/components/Device.vue
+++ ndsr-app/src/components/Device.vue
@@ -31,11 +31,11 @@
             <button @click="showWanTypesModal" :disabled="isLoading || isOffline" type="button" class="btn btn-sm btn-outline-secondary">
               <span>WAN connection type</span>
             </button>
-            <div class="btn-group flex-wrap">
+          </div>
+          <div class="btn-group flex-wrap">
             <button @click="vncOpen" type="button" class="btn btn-sm btn-outline-secondary">
               <span>Remote Desktop</span>
             </button>
-            </div>
           </div>
         </div>
       </div>
