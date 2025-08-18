63f0b3ddc7a5497392d682c091d7f3e3a03afa53
v.urusov <v.urusov@ndmsystems.com>
2025-08-18 22:04:31 +0300

    Косметические изменения. Выключили кнопки если устройство не забронировано.

--- ndsr-app/src/components/Device.vue
+++ ndsr-app/src/components/Device.vue
@@ -22,7 +22,9 @@
           <div style="font-size: 12px; color: white; width: 148px;">
             <strong>Extensions connected:</strong> {{ connectedExtensions }}
             <br>
+            <span v-if="localBookingStatus.accessPassword">
             <strong>Device Password:</strong> <span style="cursor:pointer;" @click="copy(localBookingStatus.accessPassword)">{{ localBookingStatus.accessPassword || 'Not set' }}</span>
+          </span>
           </div>
         </template>
         <template v-else #content>
@@ -54,7 +56,7 @@
       v{{ firmwareVersion }}
     </span>
   </div>
-   <div v-if="isApplyingChanges" class="spinner-border spinner-border-sm position-absolute text-white"  style="top:35px; left:10px"></div>
+   <div v-if="isApplyingChanges" class="spinner-border spinner-border-sm position-absolute text-white"  style="top:10px; left:100px"></div>
    <div v-if="localBookingStatus.isBooked" class="position-absolute top-0 end-0 m-2 d-flex flex-wrap gap-1 align-items-center">
   <!-- Основной бейдж -->
     <span class="badge badge-locked-timer text-dark flex-shrink-0">
@@ -131,7 +133,7 @@
       <button
       v-if="device.type === 'router'" 
       @click="openModal('wanTypes')" 
-      :disabled="isLoading || isOffline || !isOwnedByCurrentUser" 
+      :disabled="isLoading || isOffline || !isOwnedByCurrentUser || localBookingStatus.bookedBy != currentUserId" 
       type="button" 
       class="btn btn-outline-primary d-flex align-items-center high-100 gap-2"
       style="height: 35px; white-space: nowrap;"> 
@@ -152,38 +154,38 @@
 
   <!-- Основные кнопки -->
   <div class="d-flex flex-wrap gap-2">
-    <button @click="consoleOpen" :disabled="!isOwnedByCurrentUser || isLoading || consoleTimer > 0 || isConsoleOpen" type="button" class="btn btn-sm btn-outline-secondary flex-grow-1">
+    <button @click="consoleOpen" :disabled="!isOwnedByCurrentUser || isLoading || consoleTimer > 0 || isConsoleOpen || localBookingStatus.bookedBy != currentUserId" type="button" class="btn btn-sm btn-outline-secondary flex-grow-1">
       <i class="bi bi-terminal me-1"></i> 
       <template v-if="consoleTimer === 0">Console</template>
       <template v-else>{{ consoleTimer }}s</template>
     </button>
-    <button @click="resetConfig" :disabled="!isOwnedByCurrentUser || isLoading" type="button" class="btn btn-sm btn-outline-danger flex-grow-1">
+    <button @click="resetConfig" :disabled="!isOwnedByCurrentUser || isLoading || localBookingStatus.bookedBy != currentUserId" type="button" class="btn btn-sm btn-outline-danger flex-grow-1">
       <i class="bi bi-arrow-counterclockwise me-1"></i> Reset config
     </button>
     
-    <button @click="rebootDevice" :disabled="isLoading || !isOwnedByCurrentUser" type="button" class="btn btn-sm btn-outline-warning flex-grow-1">
+    <button @click="rebootDevice" :disabled="isLoading || !isOwnedByCurrentUser || localBookingStatus.bookedBy != currentUserId" type="button" class="btn btn-sm btn-outline-warning flex-grow-1">
       <i class="bi bi-power me-1"></i> Reboot
     </button>
     
-    <button v-if="device.dslPort" @click="resetDslLine" :disabled="isLoading || isOffline || !isOwnedByCurrentUser" type="button" class="btn btn-sm btn-outline-secondary flex-grow-1">
+    <button v-if="device.dslPort" @click="resetDslLine" :disabled="isLoading || isOffline || !isOwnedByCurrentUser || localBookingStatus.bookedBy != currentUserId" type="button" class="btn btn-sm btn-outline-secondary flex-grow-1">
       <i class="bi bi-phone me-1"></i> Reset DSL line
     </button>
   </div>
   <!-- Специальные кнопки -->
   <div class="d-flex flex-wrap gap-2">
-    <button v-if="device.dslPort === 'yes'" :disabled="!isOwnedByCurrentUser" @click="openModal('dslSettings')" type="button" class="btn btn-sm btn-outline-info flex-grow-1">
+    <button v-if="device.dslPort === 'yes'" :disabled="!isOwnedByCurrentUser || localBookingStatus.bookedBy != currentUserId" @click="openModal('dslSettings')" type="button" class="btn btn-sm btn-outline-info flex-grow-1">
       <i class="bi bi-gear me-1"></i> Setup DSL
     </button>
     
-    <button v-if="device.type === 'AP'" :disabled="!isOwnedByCurrentUser" @click="openModal('mwsConnection')" type="button" class="btn btn-sm btn-outline-success flex-grow-1">
+    <button v-if="device.type === 'AP'" :disabled="!isOwnedByCurrentUser || localBookingStatus.bookedBy != currentUserId" @click="openModal('mwsConnection')" type="button" class="btn btn-sm btn-outline-success flex-grow-1">
       <i class="bi bi-router me-1"></i> MWS Connection
     </button>
   
-    <button v-if="device.type === 'router'" @click="vncOpen" :disabled="isLoading || isOffline || !isOwnedByCurrentUser" type="button" class="btn btn-sm btn-outline-secondary flex-grow-1">
+    <button v-if="device.type === 'router'" @click="vncOpen" :disabled="isLoading || isOffline || !isOwnedByCurrentUser || localBookingStatus.bookedBy != currentUserId" type="button" class="btn btn-sm btn-outline-secondary flex-grow-1">
       <i class="bi bi-display me-1"></i> LAN VNC
     </button>
     
-    <button v-if="device.type === 'router'" @click="initializationDevice(todayPassword)" :disabled="isLoading || isOffline || !isOwnedByCurrentUser" type="button" class="btn btn-sm btn-outline-warning flex-grow-1">
+    <button v-if="device.type === 'router'" @click="initializationDevice(todayPassword)" :disabled="isLoading || isOffline || !isOwnedByCurrentUser || localBookingStatus.bookedBy != currentUserId" type="button" class="btn btn-sm btn-outline-warning flex-grow-1">
       <i class="bi bi-toggle-off me-1"></i> Disable EasyConfig
     </button>
     <!-- <button v-if="device.type === 'router'" @click="authTodevice(todayPassword)" :disabled="isLoading || isOffline || !isOwnedByCurrentUser" type="button" class="btn btn-sm btn-outline-primary flex-grow-1">
